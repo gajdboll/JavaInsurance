0e912c3733c03f35b53e46955d98d5ac8c97b7ff
kaszaninho <bartosz.wasko93@gmail.com>
2022-04-21 20:04:01 +0100

    Jaja jak berety

--- src/main/java/com/mycompany/insurancejava/NewClass.java
+++ src/main/java/com/mycompany/insurancejava/NewClass.java
@@ -6,7 +6,7 @@ public class NewClass {
     public static void main(String [] args)
             {
             System.out.println("Testowa klasa dla Bartka i Krzyska");
-            System.out.println("Testowa klasa dla Bartka i Krzyska");
+            System.out.println("Testowa klasa dla Bartka i Krzyskax");
             }
     
 }
