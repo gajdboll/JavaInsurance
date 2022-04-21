0faa74fe89a38156c67678544c7520183b7b4fb9
DESKTOP-18JG2A7\Gajdzio <confesor123@gmail.com>
2022-04-21 19:20:07 +0100

    test1

---
+++ src/main/java/com/mycompany/insurancejava/NewClass.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.insurancejava;
+
+public class NewClass {
+    
+    public static void main(String [] args)
+            {
+            System.out.println("Testowa klasa dla Bartka i Krzyska");
+            }
+    
+}
